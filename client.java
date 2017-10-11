d3d7bacb1d4c55b0d830087516373d0c823715a1
dari-o <bozhidara.ognyanova@gmail.com>
2017-10-11 15:22:21 +0200

    updates on client

--- client.java
+++ client.java
@@ -1,59 +1,99 @@
 import java.io.*;
 import java.net.*;
-public static int valueSize = 64;
-public class EchoClient{
-	public static void main(String[] args) throws IOException{
-		if(args.length != 2){
-			System.err.println("blah blah");
-			System.exit(1);
-		}
-		String hostName = args[0];
-		int portNumber = Integer.parseInt(args[1]);
-		try(
-				Socket echoSocket = new Socket(hostName, portNumber);
-				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-				BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
-				){
-			String userInput;
-			sendCommand(userInput);
-			getServerResponse(in);
-			while((userInput = stdIn.readLine()) != null){
-				sendCommand(userInput);
-			}
-		} catch (UnknownHostException e){
-			System.err.println("Don't know about host " + hostName);
-			System.exit(1);
-		}catch (IOException e){
-			System.err.println("Couldn't get I/O for the connection to " + hostName);
-			System.exit(1);
-		}
-	}
-	public void sendCommand(String userInput){
-		if(userInput.startsWith('set')){
-			boolean isSet = set(userInput);
-		}else if(userInput.startsWith('get'))
-			String data = get(userInput);
 
+public class client{
+    public static int valueSize = 64;
+    public static void main(String[] args) throws IOException{
+	if(args.length != 2){
+	    System.err.println("blah blah");
+	    System.exit(1);
 	}
-
-	public String get(String key){
-		out.println(userInput);
-		String value = in.readline();
-		boolean inDb = true;
-		if (value.equals('END')){
-			inBd = false;
-		}else{
-			String data = in.readLine();
-			in.readLine();
-			System.out.println("Server: " + value);
-			System.out.println("Server: " + data);
-		}
+	String hostName = args[0];
+	int portNumber = Integer.parseInt(args[1]);
+	try(
+	    Socket echoSocket = new Socket(hostName, portNumber);
+	    PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+	    BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+	    BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+	    ){
+	    String userInput;
+	    
+	    while((userInput = stdIn.readLine()) != null){
 		
-	}	
-	public boolean set(String userInput, String value){
-		String value = stdIn.readLine();
-		String key = userInput.split(' ')[0];
-		int hashedKey = key.hashCode();
+		sendCommand(userInput, in, out, stdIn);
+		System.out.println("Enter a command");
+	    }
+	} catch (UnknownHostException e){
+	    System.err.println("Don't know about host " + hostName);
+	    System.exit(1);
+	}catch (IOException e){
+	    System.err.println("Couldn't get I/O for the connection to " + hostName);
+	    System.exit(1);
 	}
+    }
+    public static void sendCommand(String userInput,BufferedReader in, PrintWriter out, BufferedReader stdIn){
+	    
+	if(userInput.startsWith("set")){
+	    boolean isSet = set(userInput, in, out, stdIn);
+	}else if(userInput.startsWith("get")){
+	    String data;
+	    System.out.println("get data");
+	    data = get(userInput, in, out, stdIn);
+	}
+
+    }
+
+    public static String get(String userInput, BufferedReader in, PrintWriter out, BufferedReader stdIn){
+	out.println(userInput);
+	String value;
+	String data = "";
+	try{
+	    value = in.readLine();
+	    boolean inDb = true;
+	    
+	    if (value.equals("END")){
+		inDb = false;
+		System.out.println(value);
+	    }else{
+		data = in.readLine();
+		//END message
+		in.readLine();
+		System.out.println("Server: " + value);
+		System.out.println("Server: " + data);
+	    }
+	}catch(IOException e){
+	    System.err.println("Couldn't read the input ");
+	    System.exit(1);
+	}
+	return data;
+    }	
+    public static boolean set(String userInput, BufferedReader in, PrintWriter out, BufferedReader stdIn){
+	boolean stored = false;
+	boolean error = false;
+	try{   
+	    
+	    String key = userInput.split(" ")[1];
+	    System.out.printf("Please enter the value with key %s\n", key);
+	    String value = stdIn.readLine();
+	    int hashedKey = key.hashCode();
+	    out.println(userInput);
+	    out.println(value + "\r");
+	    String result;
+	    result = in.readLine();
+	    System.out.println("Server: " + result);
+	    if(result.equals("STORED")) stored = true;
+	    else error = true;
+	    //out.println("\n");
+	    //END message
+	    //in.readLine();
+	//TO DO:
+	}catch(IOException e){
+	    System.err.println("Couldn't read the input ");
+	    System.exit(1);
+	}
+	return stored; 
+    }
+    public static boolean isError(String serverResponse){
+	return false;
+    }
 }
